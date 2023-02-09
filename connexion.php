0416f0bfccf202ea1b279839792eff6effb2a405
Karima <Karima.akrache@icloud.com>
2023-02-09 17:11:17 +0100

    second commit

--- connexion.php
+++ connexion.php
@@ -42,12 +42,12 @@
 
                   }//fin du if verif mdp
                   else{
-                      $erreur = 'Votre email ou votre mot de passe est incorrecte.';
+                      $erreur = 'Votre email ou votre mot de passe est incorrecte, veuillez reesayer!';
                       //le mdp est incorrecte, pour des raisons de sécurité et de differences entre les navigateurs, mettre "email et mdp incorrect" 
                   } 
               }//fin du if correspondance
               else{
-                      $erreur = 'Votre email ou votre mot de passe est incorrecte.'; 
+                      $erreur = 'Votre email ou votre mot de passe est incorrecte, veuillez reesayere!'; 
                       //l'email est incorrecte, pour des raisons de sécurité et de differences entre les navigateurs, mettre "email et mdp incorrect" 
                   } 
             
